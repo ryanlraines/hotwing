c52c3109b07c6bc19e1b704cf58ddf0275f8e1d4
ryanlraines <ryanlraines@gmail.com>
2021-01-27 22:41:51 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# hotwing
+# Summary
+//TODO
+
+# Requirements
+//TODO
+
+# Start up
+1. `docker build .`
+
+1. `docker run -p 3000:3000 <imageid>`
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({  "message": "Automate all the things!",  "timestamp": Date.now()}));
+});
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+  });
