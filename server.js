37400cae740dd8d8b66d7d6860f9816ee8813d06
ryanlraines <ryanlraines@gmail.com>
2021-01-31 22:03:16 -0500

    changed to port 80

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
+    console.log(`app listening at http://localhost`)
   });
