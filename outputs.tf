c8a065e03e3ba3ff47a4795b4592a3e7e5f54c56
ryanlraines <ryanlraines@gmail.com>
2021-01-31 20:27:19 -0500

    removed extra vars

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
-}
-
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
   value       = module.eks.cluster_endpoint
@@ -26,9 +21,4 @@ output "config_map_aws_auth" {
 output "region" {
   description = "AWS region"
   value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
 }
