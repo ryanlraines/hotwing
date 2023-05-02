4fd8a648c22e91240f10a65e531b9ce9b62475b6
Ryan Raines <ryan.raines@orbitalinsight.com>
2023-05-02 18:19:57 -0400

    add backend

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,11 @@ variable "region" {
 variable "cluster_name" {
   default = "hotwing-cluster"
 }
+
+variable "s3_backend" {
+  default = "general-raines"
+  }
+
+variable "state_table" {
+  default = "tf_state"
+}
